5c7991376ba38b8b64f62b6defb46d68f192c4d8
AmanKamani <kamaniamanak@gmail.com>
2021-10-15 21:49:02 +0530

    feat(dependencies): Added Hilt & Navigation Dependencies
    - Prepare setup for Hilt.

---
+++ app/src/main/java/jb/production/fitsleep/FitSleepApplication.kt
@@ -0,0 +1,8 @@
+package jb.production.fitsleep
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FitSleepApplication: Application() {
+}
---
+++ app/src/main/java/jb/production/fitsleep/di/ApplicationModule.kt
@@ -0,0 +1,10 @@
+package jb.production.fitsleep.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+object ApplicationModule {
+}
