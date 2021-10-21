a4f19e94c59bee0f6da8cff9e1117cfdcd84dfe8
AmanKamani <kamaniamanak@gmail.com>
2021-10-21 22:18:24 +0530

    feat(chart): Added basic pie chart for testing

--- app/src/main/java/jb/production/fitsleep/MainActivity.kt
+++ app/src/main/java/jb/production/fitsleep/MainActivity.kt
@@ -1,11 +1,32 @@
 package jb.production.fitsleep
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val pie = AnyChart.pie()
+
+        val data: MutableList<DataEntry> = ArrayList()
+        data.add(ValueDataEntry("John", 10000))
+        data.add(ValueDataEntry("Jake", 12000))
+        data.add(ValueDataEntry("Peter", 18000))
+
+        pie.data(data)
+
+        val anyChartView = findViewById<View>(R.id.any_chart_view) as AnyChartView
+        anyChartView.setChart(pie)
     }
 }
